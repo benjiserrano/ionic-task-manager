a99fd94830d9d0335dbc7d7fa1d0836cdfe4677a
Benji <bjserranoweb@gmail.com>
2023-12-19 19:00:27 +0100

    refactor: Mejora de código
    - Movida función getTags() al servicio Tag
    - Cambio de TaskStatus a Status
    - Cambio de propiedad "tagIds" a "tags"

--- src/app/models/Task.ts
+++ src/app/models/Task.ts
@@ -1,23 +1,14 @@
-import { Tag, DefaultTags } from "./Tag";
-
 export interface Task {
     id: number;
     title: string;
     description: string;
-    status: TaskStatus;
-    tagIds: number[]; // Array de identificadores de las etiquetas asociadas a la tarea
-    getTags(): Tag[];
-  }
-  
-export enum TaskStatus {
-  pending = 'Pendiente',
-  in_progress = 'En progreso',
-  completed = 'Completada'
+    status: string;
+    tags: number[]; // Array de identificadores de las etiquetas asociadas a la tarea
 }
 
-export function getTags(this: Task): Tag[] {
-  // Lógica para cargar las etiquetas correspondientes a partir de los tagIds
-  return this.tagIds
-    .map(tagId => DefaultTags.find(tag => tag.id === tagId)) // Intenta encontrar la etiqueta correspondiente
-    .filter(tag => tag !== undefined) as Tag[]; // Filtra los elementos undefined del array resultante
+export enum Status {
+  pending = 'Pendiente',
+  in_progress = 'En Progreso',
+  completed = 'Completada',
+  canceled = 'Cancelada'
 }
