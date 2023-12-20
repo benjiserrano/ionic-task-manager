fb662c5869ed28a164f22d58c9115167750cacf7
Benji <bjserranoweb@gmail.com>
2023-12-20 17:29:01 +0100

    feat: Página de perfil
    Se ha integrado un botón para cerrar la sesión y comprobar la funcionalidad.

---
+++ src/app/perfil/perfil.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+}
