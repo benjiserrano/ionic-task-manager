6ec5018fc1ccadb60d343fbbcb010dd740758f19
Benji <bjserranoweb@gmail.com>
2023-12-20 17:18:54 +0100

    feat: Implementar AuthGuard en el enrutamiento

--- src/app/pages/tareas/crear/crear-routing.module.ts
+++ src/app/pages/tareas/crear/crear-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CrearPage } from './crear.page';
+import { AuthGuard } from 'src/app/services/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: CrearPage
+    component: CrearPage,
+    canActivate: [AuthGuard]
   }
 ];
 
