f1ae7f5e2e7b8abe9216ccbdd988fe9c1938b44f
Benji <bjserranoweb@gmail.com>
2024-02-09 17:08:45 +0100

    test: speechSynthesis

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -11,10 +11,28 @@ export class Home {
 
   auth_name: string | null = 'usuario';
 
-  constructor(public taskService: TaskService) {}
+  constructor(public taskService: TaskService) {
+    
+    
+    setTimeout(() => {
+      window.speechSynthesis.onvoiceschanged = function() {
+        const voices = window.speechSynthesis.getVoices();
+        const message = "Buenas noches Bénlli. Tienes 5 nuevos correos en la bandeja de entrada.";
+        let utterance = new SpeechSynthesisUtterance(message);
+        //Microsoft Alvaro Online (Natural) - Spanish (Spain)
+        utterance.pitch = 0.1;
+        utterance.voice = voices[2];
+        window.speechSynthesis.speak(utterance);
+      };
+    }, 4000);
+  }
   
   ngOnInit(): void {
     console.log(localStorage.getItem('isAuthenticated'))
     this.auth_name = localStorage.getItem('name');
+    
+    
   }
+
+  
 }
